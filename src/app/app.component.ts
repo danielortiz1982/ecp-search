20af3cb5c8d09d5172ec22bfcc5308f94a2304c8
Daniel Ortiz <dortiz-consultant@scholastic.com>
2020-02-07 16:47:07 -0500

    adding search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { HttpClient }    from '@angular/common/http'
+import { FormsModule }   from '@angular/forms'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'ecp-search';
+    export class AppComponent implements OnInit{
+    public search: any = ''
+    ecpResults: any []
+    scholasticLogo: string = '../assets/img/scholastic-logo.png'
+    usersEndpoint: string = 'https://jsonplaceholder.typicode.com/users'
+
+    getUsers(){
+      this.http.get<any>(this.usersEndpoint).subscribe( users => {
+        users.forEach( el => this.ecpResults.push(el) )
+      })
+    }
+
+    constructor(private http: HttpClient){}
+
+    ngOnInit(){
+        this.ecpResults = []
+        this.getUsers()
+    }
 }
---
+++ src/app/search.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core'
+
+@Pipe({
+    name: 'EcpSearchFilter'
+})
+
+export class SearchPipe implements PipeTransform {
+    transform( value: any, args ? : any ): any {
+        if(!value)return null
+        if(!args)return value
+        args = args.toLowerCase()
+        return value.filter( item => JSON.stringify(item).toLowerCase().includes(args))
+    }
+}
